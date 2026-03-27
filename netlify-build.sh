57bf8295183396b0fd1cbabad1887398d9e1e69d
Backershan T <98761199+BackershanT@users.noreply.github.com>
2026-03-28 00:21:01 +0530

    upd

---
+++ netlify-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# Install Flutter SDK into $HOME/flutter if not present
+if [ ! -d "$HOME/flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
+fi
+
+export PATH="$HOME/flutter/bin:$PATH"
+
+# Force Flutter to download required artifacts
+flutter --version
+flutter doctor -v
+
+# Get packages and build web
+flutter clean
+rm -rf ~/.pub-cache
+flutter pub get
+flutter build web --release
